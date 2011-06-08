1e8fcb6a32638eb6724019975c2cf095e0920ef2
Hubert <hubert.lepicki@amberbit.com>
2011-06-08 16:15:19 +0200

    use mongodb for development

--- spec/dummy/config/initializers/translator.rb
+++ spec/dummy/config/initializers/translator.rb
@@ -1,8 +1,10 @@
-#conn = Mongo::Connection.new.db("translator_test").collection("translations")
-#Translator.current_store = Translator::MongoStore.new(conn)
-#Translator.current_store = Translator::RedisStore.new(Redis.new)
+if Rails.env.development?
+  conn = Mongo::Connection.new.db("translator_test").collection("translations")
+  Translator.current_store = Translator::MongoStore.new(conn)
+  Translator.current_store = Translator::RedisStore.new(Redis.new)
 
-#I18n.backend = Translator.setup_backend(I18n::Backend::Simple.new)
+  I18n.backend = Translator.setup_backend(I18n::Backend::Simple.new)
+end
 
 #Translator.auth_handler = proc {
 #  authenticate_or_request_with_http_basic do |user_name, password|
