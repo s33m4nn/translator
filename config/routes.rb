7451fc9b50de2b40ff94fa4a4e35f3ec428a7ee7
Roman Shterenzon <roman.shterenzon@gmail.com>
2012-08-18 17:02:08 +0300

    Make the translator into a true Rails::Engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  resources :translations, :to => "Translator::Translations"
+# When Rails >= 3.1
+if defined?(Translator::Engine)
+  Translator::Engine.routes.draw do
+    resources :translations
+  end
+else
+  Rails.application.routes.draw do
+    resources :translations, :to => "Translator::Translations"
+  end
 end
